1c182fa9a7531ea24c560ddb78f222fe7cc73792
Daniel Dickman <ddickman@college.harvard.edu>
2025-03-10 23:53:55 -0400

    converted timeout to use relaxed_atomic

--- mttest.cc
+++ mttest.cc
@@ -74,7 +74,7 @@
 #include "atomic_pthread.hh"
 
 static std::vector<int> cores;
-volatile bool timeout[2] = {false, false};
+relaxed_atomic<bool> timeout[2] = {false, false};
 double duration[2] = {10, 0};
 int kvtest_first_seed = 31949;
 uint64_t test_limit = ~uint64_t(0);
